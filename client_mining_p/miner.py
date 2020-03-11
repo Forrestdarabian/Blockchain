2b8146456ee09636365a3605b458fae08dff8614
Forrest-Darabian <mr.smiley72@yahoo.com>
2020-03-11 13:46:07 -0700

    working on miner.py

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -8,12 +8,16 @@ import json
 def proof_of_work(block):
     """
     Simple Proof of Work Algorithm
-    Stringify the block and look for a proof.
-    Loop through possibilities, checking each one against `valid_proof`
-    in an effort to find a number that is a valid proof
-    :return: A valid proof for the provided block
     """
-    pass
+    # Stringify the block and look for a proof.
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    # Loop through possibilities, checking each one against `valid_proof`
+    # in an effort to find a number that is a valid proof
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    # :return: A valid proof for the provided block
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +31,10 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == "000000"
 
 
 if __name__ == '__main__':
@@ -56,7 +63,7 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        new_proof = proof_of_work(data.get('last_block'))
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
